4d9af69444ec0d96ec7e0f6b9dadaedd8db58c8b
CHENG HAN <y3226999@gmail.com>
2021-01-20 16:02:32 +0800

    heap

--- Swap.c
+++ Swap.c
@@ -6,11 +6,27 @@ void swap(int *a, int *b){
 	*b = temp;
 }
 
-void main(){
-	int a = 10,
-		b = 5;
+void arrPointer(int *a){
+	int i;
+	int *p;
+	
+	p = a;
+	
+	for(i = 0; i < 10; ++i)
+		a[i] = i;
 		
-	swap(&a, &b);	
-	printf("a = %d\nb = %d\n", a, b);
+	for(i = 0; i < 10; ++i)
+		printf("a[%d] = %d, p[%d] = %d\n", i, a[i], i, p[i]);
+}
+
+void main(){
+	int i;
+	int a[10];
+//	int *p = &a[0] + 5;
+	
+	arrPointer(a);
+	
+	//swap(&a, &b);	
+	//printf("a = %d\nb = %d\n", a, b);
 	return 0;
 } 
