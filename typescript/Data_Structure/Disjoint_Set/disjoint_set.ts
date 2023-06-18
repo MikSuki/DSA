4225edb23280c0acffebdf3936638b0ca3689499
MikSuki <34766025+MikSuki@users.noreply.github.com>
2023-06-18 19:14:12 +0800

    upd disjoint_set

--- typescript/Data_Structure/Disjoint_Set/disjoint_set.ts
+++ typescript/Data_Structure/Disjoint_Set/disjoint_set.ts
@@ -1,4 +1,4 @@
-class Node<T> {
+export class Node<T> {
     public parent: Node<T>
     public size: number
     private data: T
@@ -25,11 +25,13 @@ export class DisjointSet<T> {
     public Union(x: Node<T>, y: Node<T>) {
         let root1: Node<T> = this.Find(x)
         let root2: Node<T> = this.Find(y)
+        if(root1 == root2) return 
 
         if(root2.size > root1.size)
             [root1, root2] = [root2, root1]
-
+       
         root2.parent = root1
         root1.size += root2.size
+        root2.size = 0
     }
 }
