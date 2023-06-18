9dec15b529a243720b5c4c69f51a3d3ae8553c90
MikSuki <34766025+MikSuki@users.noreply.github.com>
2023-06-18 19:29:29 +0800

    Update disjoint_set.ts

--- typescript/Data_Structure/Disjoint_Set/disjoint_set.ts
+++ typescript/Data_Structure/Disjoint_Set/disjoint_set.ts
@@ -1,3 +1,17 @@
+/**
+ * - Disjoint-set 是用來處理沒有重複元素的問題，
+ *   其中包含了Find和Union
+ *   
+ * - 路徑壓縮(path compression)
+ *      每次Find時，把經過的node之parent設為root
+ *      藉此減少樹的高度
+ * 
+ * - Time Complexity
+ *      MakeSet: O(1)
+ *      Find: O(α(n))
+ *      Union: O(α(n))
+ */
+
 export class Node<T> {
     public parent: Node<T>
     public size: number
