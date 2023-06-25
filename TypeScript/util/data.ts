098bce67439be8060190d4897f62193ac7cd3fa9
MikSuki <34766025+MikSuki@users.noreply.github.com>
2023-06-25 15:01:57 +0800

    Update data.ts

--- TypeScript/util/data.ts
+++ TypeScript/util/data.ts
@@ -28,7 +28,7 @@ export function sorted_repeat(alphabet_size: number, size: number): number[] {
     for (let i = 0; i < size; ++i)
         arr.push(~~(Math.random() * alphabet_size))
     arr.sort((a, b) => a - b)
-    return arr
+    return arr  
 }
 
 /**
@@ -45,3 +45,34 @@ export function nonSorted_nonRepeat(alphabet_size: number, size: number): number
     return arr
 }
 
+/**
+ * check if the input sequence is sorted
+ * @param input 
+ * @returns 
+ */
+export function isSorted(input: number[]): boolean{
+    console.log(input)
+    for(let i = 1; i < input.length; ++i)
+        if(input[i] < input[i - 1]) return false
+    return true
+}
+
+/**
+ * check if two input are same
+ * @param a 
+ * @param b 
+ * @returns 
+ */
+export function isSame(a: number | number[], b: number | number[]){
+    if(typeof a == 'number' && typeof b == 'number'){
+        return a == b
+    }
+    if(a instanceof Array && b instanceof Array){
+        if(a.length != b.length) return false
+        for(let i = 0; i < a.length; ++i)
+            if(isSame(a[i], b[i]) == false)
+                return false
+        return true
+    }
+    throw "type error"
+}
