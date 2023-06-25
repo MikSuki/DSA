5a8deb6574e7c04250600c76669ed43b6d8814ac
MikSuki <34766025+MikSuki@users.noreply.github.com>
2023-06-25 15:26:03 +0800

    update function name

--- TypeScript/Algorithm/Sort/test/insertion_sort.test.ts
+++ TypeScript/Algorithm/Sort/test/insertion_sort.test.ts
@@ -1,5 +1,5 @@
 import {insertionSort} from "../insertion_sort"
-import {nonSorted_nonRepeat, isSame} from "util/data" 
+import {data_Sorted_Repeated, isSame} from "util/data" 
 
 const alpha: number = 1000
 const size: number = 1000
@@ -8,7 +8,7 @@ const time: number = 1000
 describe('insertion sort', () => {
    test('zz', () => {
         for(let i = 0; i < time; ++i){
-            const data1 = nonSorted_nonRepeat(alpha, size)
+            const data1 = data_Sorted_Repeated(alpha, size)
             const data2 = data1.slice()
             insertionSort(data1)
             data2.sort((a, b) => a - b)
@@ -16,4 +16,3 @@ describe('insertion sort', () => {
         }
    }) 
 })
-
--- TypeScript/util/data.ts
+++ TypeScript/util/data.ts
@@ -5,55 +5,59 @@
  * @param size size of data
  * @returns 
  */
-export function sorted_nonRepeat(start: number, gap: number, size: number): number[] {
+export function nonRepeat(start: number, gap: number, size: number, sort: boolean): number[] {
     const arr: number[] = []
     let cur = start
 
-    for (let i = 0; i < size; ++i){
+    for (let i = 0; i < size; ++i) {
         cur += ~~(Math.random() * gap) + 1
         arr.push(cur)
     }
+    if (sort) arr.sort((a, b) => a - b)
     return arr
 }
 
 /**
- * generate repeated sorted data
+ * generate non-sorted repeated data
  * @param alphabet_size maximum of data
  * @param size size of data
+ * @param sort need sort
  * @returns 
  */
-export function sorted_repeat(alphabet_size: number, size: number): number[] {
+export function data_NonSorted_Repeated(alphabet_size: number, size: number): number[] {
     const arr: number[] = []
+    
+    for (let i = 0; i < size; ++i)
+        arr.push(~~(Math.random() * alphabet_size))
+    return arr
+}
 
+/**
+ * generate sorted repeated data
+ * @param alphabet_size maximum of data
+ * @param size size of data
+ * @param sort need sort
+ * @returns 
+ */
+export function data_Sorted_Repeated(alphabet_size: number, size: number): number[] {
+    const arr: number[] = []
+    
     for (let i = 0; i < size; ++i)
         arr.push(~~(Math.random() * alphabet_size))
     arr.sort((a, b) => a - b)
-    return arr  
-}
-
-/**
- * generate non-repeated non-sorted data
- * @param alphabet_size maximum of data
- * @param size size of data
- * @returns 
- */
-export function nonSorted_nonRepeat(alphabet_size: number, size: number): number[] {
-    const arr: number[] = []
-
-    for (let i = 0; i < size; ++i)
-        arr.push(~~(Math.random() * alphabet_size))
     return arr
 }
+
 
 /**
  * check if the input sequence is sorted
  * @param input 
  * @returns 
  */
-export function isSorted(input: number[]): boolean{
+export function isSorted(input: number[]): boolean {
     console.log(input)
-    for(let i = 1; i < input.length; ++i)
-        if(input[i] < input[i - 1]) return false
+    for (let i = 1; i < input.length; ++i)
+        if (input[i] < input[i - 1]) return false
     return true
 }
 
@@ -63,14 +67,14 @@ export function isSorted(input: number[]): boolean{
  * @param b 
  * @returns 
  */
-export function isSame(a: number | number[], b: number | number[]){
-    if(typeof a == 'number' && typeof b == 'number'){
+export function isSame(a: number | number[], b: number | number[]) {
+    if (typeof a == 'number' && typeof b == 'number') {
         return a == b
     }
-    if(a instanceof Array && b instanceof Array){
-        if(a.length != b.length) return false
-        for(let i = 0; i < a.length; ++i)
-            if(isSame(a[i], b[i]) == false)
+    if (a instanceof Array && b instanceof Array) {
+        if (a.length != b.length) return false
+        for (let i = 0; i < a.length; ++i)
+            if (isSame(a[i], b[i]) == false)
                 return false
         return true
     }
